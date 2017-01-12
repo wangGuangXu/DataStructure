55cd565bc75f7376cd725d0f87a7d80cf1a22d92
wgx0428 <656052658@qq.com>
2017-01-12 16:59:02 +0800

    计算1+2+3+……100数之和

--- 数据结构01/数据结构01/Program.cs
+++ 数据结构01/数据结构01/Program.cs
@@ -9,6 +9,27 @@ namespace 数据结构01
     {
         static void Main(string[] args)
         {
+            //计算：1+2+3+……100的和
+
+            ////最普通的计算方法
+            //int num = 0;
+            //for (int i = 1; i <= 100; i++)
+            //{
+            //    num =num+i;
+            //}
+            //Console.WriteLine(num);
+
+            /* 数学家高斯的解题思路：
+             *  sum=1+2+3+4+5……+100 
+             * sum2=100+99+98+97+96+……+1
+             * sum3=101+101+101+……+101 正好为50个
+             * sum4=101*50
+             */
+            int n = 100;
+            int sum = (1 + n) * n / 2;
+            Console.WriteLine(sum);
+
+            Console.Read();
         }
     }
 }
