933886ac22b278d7df92607858087f8838e317b4
wgx0428 <656052658@qq.com>
2018-11-20 10:02:56 +0800

    添加数据结构大分类

---
+++ 数据结构01/内置排序01/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace 内置排序01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[] { 30, 21, 60, 9, 58, 140, 1 };
+
+            //采用系统内置的数组排序
+            var sortArray = array.OrderBy(a => a).ToArray();
+            for (int i = 0; i < sortArray.Length; i++)
+            {
+                Console.WriteLine(sortArray[i]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 数据结构01/冒泡排序01/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace 冒泡排序01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[] { 30, 21, 60, 9, 58, 140, 1 };
+
+            //for (int i = 0; i < array.Length; i++)
+            //{
+            //    for (int i = 0; i < length; i++)
+            //    {
+
+            //    }
+            //}
+            Console.ReadKey();
+        }
+    }
+}
